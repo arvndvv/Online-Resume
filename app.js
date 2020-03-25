0588c50f22988b73553f648db414a0551a754c48
Aravind V V <aravind16498@gmail.com>
2020-03-25 10:26:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,3 +62,16 @@ function checkAnimation() {
 $(window).scroll(function() {
   checkAnimation();
 });
+
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+
+    document.querySelector("body").classList.remove("hid");
+  }
+};
